d04fb171c54fca7782cec68334b80b658c51fcea
SaimonNeupane <Saimonbro00007@gmail.com>
2025-03-13 10:47:08 +0545

    Add error handlers

--- server/server.js
+++ server/server.js
@@ -2,13 +2,20 @@ import express from "express";
 import bodyParser from "body-parser";
 import { configDotenv } from "dotenv";
 import morgan from "morgan";
+import mongoose from "mongoose";
+import GlobalError from "./Errors/GlobalError.js";
 
 configDotenv();
 const app = express();
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.log(`couldnot connect to databse ${err.message}`));
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
+app.use(GlobalError);
 app.listen(process.env.PORT_NUMBER, () => {
   console.log(
     `app currently listening on port number ${process.env.PORT_NUMBER}`
