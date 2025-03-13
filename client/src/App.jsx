f34ac62409a65b4077bf79850dec6da288e050d2
Surya Shashank Neupane <suryaneupane0427@gmail.com>
2025-03-13 13:26:33 +0545

    Remove hardcoded marker data from App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,48 +5,6 @@ import "leaflet/dist/leaflet.css";
 import "leaflet.awesome-markers";
 import { useEffect, useState } from "react";
 import { getAllPlaces } from "../utils/Api";
-const markers = [
-  {
-    geocode: [28.3949, 84.124], // Gandaki Province (Pokhara)
-    popUp: "Pokhara",
-  },
-  {
-    geocode: [27.7172, 85.324], // Bagmati Province (Kathmandu)
-    popUp: "Kathmandu",
-  },
-  {
-    geocode: [26.455, 87.283], // Province 1 (Biratnagar)
-    popUp: "Biratnagar",
-  },
-  {
-    geocode: [27.6766, 83.4323], // Lumbini Province (Butwal)
-    popUp: "Butwal",
-  },
-  {
-    geocode: [28.0159, 81.6031], // Sudurpashchim Province (Dhangadhi)
-    popUp: "Dhangadhi",
-  },
-  {
-    geocode: [28.2724, 83.5982], // Gandaki Province (Gorkha)
-    popUp: "Gorkha",
-  },
-  {
-    geocode: [27.0, 84.8667], // Madhesh Province (Janakpur)
-    popUp: "Janakpur",
-  },
-  {
-    geocode: [29.0, 81.25], // Karnali Province (Jumla)
-    popUp: "Jumla",
-  },
-  {
-    geocode: [27.5, 85.3333], // Bagmati Province (Bhaktapur)
-    popUp: "Bhaktapur",
-  },
-  {
-    geocode: [27.63, 83.87], // Lumbini Province (Lumbini)
-    popUp: "Lumbini",
-  },
-];
 
 function App() {
   const [data, setData] = useState(null);
