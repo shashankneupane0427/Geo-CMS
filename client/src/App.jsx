cf7f1d745cee7fc6c5c247176d49b4dc6cecba7c
Surya Shashank Neupane <suryaneupane0427@gmail.com>
2025-03-13 13:08:41 +0545

    Add react-leaflet-cluster for marker clustering in the map

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { Icon } from "leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
 import "leaflet/dist/leaflet.css";
+
 import "leaflet.awesome-markers";
 
 const markers = [
@@ -49,18 +50,23 @@ const markers = [
 function App() {
   return (
     <>
-      <MapContainer center={[28.3949, 84.124]} zoom={7}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {/* Mapping through the markers */}
-        {markers.map((marker, index) => (
-          <Marker key={index} position={marker.geocode}>
-            <Popup>{marker.popUp}</Popup>
-          </Marker>
-        ))}
-      </MapContainer>
+      <div className="">
+        <h1 className="text-center text-3xl font-bold text-gray-800 mt-10"></h1>
+        <MapContainer center={[28.3949, 84.124]} zoom={7}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <MarkerClusterGroup chunkedLoading>
+            {/* Mapping through the markers */}
+            {markers.map((marker, index) => (
+              <Marker key={index} position={marker.geocode}>
+                <Popup>{marker.popUp}</Popup>
+              </Marker>
+            ))}
+          </MarkerClusterGroup>
+        </MapContainer>
+      </div>
     </>
   );
 }
